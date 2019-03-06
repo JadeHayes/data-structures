72fdc1d14292b25fcdd8ccb4c1f1f61604f51607
Jade Hayes <Jade.e.Hayes@gmail.com>
2019-03-05 22:23:10 -0800

    * Stack notes.

---
+++ 3_Basic_Data_Structures/3.3_Stacks.py
@@ -0,0 +1,10 @@
+# what distinguishes one linear structure from another is the
+# way in which items are added and removed, in particular the location where these additions and removals occur.
+
+# Stack (like a stack of pancakes) items are removed from the top and added from the top
+#  Items stored at the base of the stack are there the longest. The most recently placed
+# Item is moved first. LIFO last-in-first-out.  Newer items are at the top and older items
+# are at the base. The order items are removed is exactly the reverse of the order that they were placed.
+# Stacks can be used to reverse the order of items.  The order of insertion is reverse the order of removal
+
+# ex: web browser back button, (as you navigate URL's get stored in a Stack)
