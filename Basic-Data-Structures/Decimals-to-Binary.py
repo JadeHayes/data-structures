87dfb2db58a861cf78cbb7942a81376d450a1513
Jade Hayes <Jade.e.Hayes@gmail.com>
2019-03-07 20:49:05 -0800

    * Updated file names.
    * Added digits to binary exercise.

---
+++ Basic-Data-Structures/Decimals-to-Binary.py
@@ -0,0 +1,25 @@
+from pythonds.basic.stack import Stack
+
+
+
+def base_converter(decimal: int, base:int) -> str:
+    digits = "0123456789ABCDEF"
+    stack = Stack()
+
+    while decimal > 0:
+        remainder = decimal % base
+        stack.push(remainder)
+        decimal = decimal // base
+
+    ans = ""
+    while not stack.isEmpty():
+        ans += digits[stack.pop()]
+
+    return ans
+
+
+base_converter(25, 2)
+base_converter(888, 18)
+base_converter(28, 11)
+base_converter(100, 12)
+base_converter(3, 8)
