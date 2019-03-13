58f2da3ff32adb7cfdcc775ccd52edfe4ef693e7
Jade Hayes <Jade.e.Hayes@gmail.com>
2019-03-12 22:15:35 -0700

    * Added 3.26. Discussion Questions.

--- Basic-Data-Structures/Expressions.py
+++ Basic-Data-Structures/Expressions.py
@@ -14,22 +14,73 @@ def infix_to_postfix(infix: str):
     token: list =infix.split()
 
     for t in token:
+        #  If the token we are on isn't an operator or (, add it to the postfix list
+        # list because the position of the letters and numbers do not change.
         if t in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or t in "0123456789":
             post_fix.append(t)
+        # If the token is a opening paren "(" it means there will be a closing
+        # paren we need to translate this to postfix, pop it on the stack.
         elif t =="(":
             opStack.push(t)
+        # if the token is a closing paren
         elif t ==")":
             top_token = opStack.pop()
             while top_token != "(":
                 post_fix.append(top_token)
                 top_token = opStack.pop()
+        # If the token is an operator, check to see if the opstack is empty and if the
+        # tokens precedence is > or = the top token on the stack.
+        # if it's greater than, it will be calculated first, so put it in the list.
         else:
             while (not opStack.isEmpty()) and (prec[opStack.peek()] >= prec[t]):
                 post_fix.append(opStack.pop())
+            # if the stack is empty or the precedence is less than, put the token on the top of the stack.
+            #  this way the lower precedence operators will get added to the list first.
             opStack.push(t)
+    # after we've gone through all of the tokens, check the stack to see what is left.
+    # pop it off in that order and add it to the list
     while not opStack.isEmpty():
         post_fix.append(opStack.pop())
+    # join the list and return
     return  " ".join(post_fix)
 
 
+def postfixEval(postfixExpr):
+    operandStack = Stack()
+    tokenList = postfixExpr.split()
+
+    # check to see if the operator depends on int order
+    def _doMath(op, op1, op2):
+        """helper function to compute the two numbers in the stack"""
+        if op == "*":
+            return op1 * op2
+        elif op == "/":
+            return op1 / op2
+        elif op == "+":
+            return op1 + op2
+        else:
+            return op1 - op2
+
+     # iterate over the list of tokens and check to see if it's a number.
+    # if it is, push it to the stack so we can save it for when we find an operator
+    for token in tokenList:
+        if token in "0123456789":
+            operandStack.push(int(token))
+        else:
+            # if it's an operator, pop the first two ints off the stack
+            operand2 = operandStack.pop()
+            operand1 = operandStack.pop()
+            # do the math necessary per that token
+            result = _doMath(token,operand1,operand2)
+            # push the result back onto the stack.
+            operandStack.push(result)
+    # return the last number that is on the stack.
+    return operandStack.pop()
+
+print(postfixEval('7 8 + 3 2 + /'))
+print(postfixEval('4 5 6 * +'))
+
+
 print(infix_to_postfix("A * B * C * D + E + F"))
+print(infix_to_postfix("A + ( ( B + C ) * ( D + E ) )"))
+print(infix_to_postfix("( A + B ) * ( C + D ) * ( E + F )"))
--- Basic-Data-Structures/discussion-questions.py
+++ Basic-Data-Structures/discussion-questions.py
@@ -1,11 +1,13 @@
 from binary import base_converter
+from Expressions import postfixEval
 
 # Convert the following values to binary using “divide by 2.” Show the stack of remainders.
 print(base_converter(17, 2))
 print(base_converter(45, 2))
 print(base_converter(96, 2))
 
-# Convert the following infix expressions to prefix (use full parentheses):
+
+"""Convert the following infix expressions to prefix (use full parentheses)."""
 # (A+B)*(C+D)*(E+F)
 # + A B * + C D* + E F
 
@@ -14,3 +16,51 @@ print(base_converter(96, 2))
 
 # A * B * C * D + E + F
 # ANS >>> + + * A  * B  * C D E F
+
+"""Convert the above infix expressions to postfix (use full parentheses)."""
+# (A+B)*(C+D)+ * E F + *
+
+# A+((B+C)*(D+E))
+#  A B C + D E + * +
+#  stack iteration  # List iteration
+#                   # A B C + D E + * +
+
+
+""" Evaluate the following postfix expressions. Show the stack as each operand and operator is processed."""
+# 2 3 * 4 +
+# 1 2 + 3 + 4 + 5 +
+# 1 2 3 4 5 * + * +
+
+print(postfixEval("2 3 * 4 +"))
+print(postfixEval('1 2 + 3 + 4 + 5 +'))
+print(postfixEval('1 2 + 3 + 4 + 5 +'))
+print(postfixEval('1 2 3 4 5 * + * +'))
+
+"""
+The alternative implementation of the Queue ADT is to use a list such 
+that the rear of the queue is at the end of the list. What would this mean for Big-O performance?
+"""
+#  If a Queue were set up as a list, where the rear is the end. Big(O) performance for the methods:
+# enqueue() -> O(n) # the rest of the list needs to shift as well
+# dequeue() -> O(1)
+# isEmpty() -> O(1)
+# size() -> O(1) # in python it's O(1) we could loop through and count for O(n)
+
+"""
+What is the result of carrying out both steps of the linked list add method in reverse order? 
+What kind of reference results? 
+What types of problems may result?
+"""
+# Carrying out the `add` method in reverse order for a linked list you would set the head and then set the
+# net node, which would break it because if the head was set before setting the current node's next,
+# the head is what connects everything else, if the head wasn't connected you'd loose your list
+
+"""Explain how the linked list remove method works when the item to be removed is in the last node."""
+# When the last item is removed from the linked list, we set a variable `previous` to check the previous node.
+# once our current Node is `None`, we know we are at the end of our linked list. We can then set our previous node's
+# next to None, and our last node is removed from the list since it is nobody's next.
+
+
+"""Explain how the remove method works when the item is in the only node in the linked list."""
+# When the remove method is removing the only item in the list, it needs to check that the next node in the list
+# is `None` and that the previous node in the list is also `None`.
